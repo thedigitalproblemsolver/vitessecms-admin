cf6f4c5e40e5e32b46864b9472c8c531d0ce1166
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-07-05 16:11:35 +0200

    Replace mongoId with Human readable value

--- src/Traits/TraitAdminModelReadOnly.php
+++ src/Traits/TraitAdminModelReadOnly.php
@@ -2,6 +2,7 @@
 
 namespace VitesseCms\Admin\Traits;
 
+use VitesseCms\Content\Enum\ItemEnum;
 use VitesseCms\Core\Utils\StringUtil;
 use VitesseCms\Mustache\DTO\RenderTemplateDTO;
 use VitesseCms\Mustache\Enum\ViewEnum;
@@ -30,46 +31,50 @@ trait TraitAdminModelReadOnly
             $vars = [];
             foreach($properties as $key => $property) {
                 if(isset($model->$key)) {
-                    $value = $model->$key;
-                    if (gettype($value) === 'object') {
-                        if ($value::class === 'MongoDB\BSON\UTCDateTime') {
-                            $value = $value->toDateTime()->format('Y-m-d H:i:s');
-                        }
-                    }
-                    switch ($key) {
-                        case 'itemId':
-                            /*var_dump((string) $value);
-                            $item = $this->itemRepository->getById((string) $value, false);
-                            var_dump($item);
-                            die();
-                            if($item !== null) {
-                                $value = $item->getNameField();
-                            }*/
-                            break;
-                        case 'userId':
-                            $user = $this->userRepository->getById((string) $value, false);
-                            if($user !== null) {
-                                $value = $user->getNameField();
-                            }
-                            break;
-                    }
-
-
                     $vars[] = [
                         'key' => ucfirst(StringUtil::camelCaseToSeperator($key)),
-                        'value' => $value
+                        'value' => $this->getReadOnlyValue($key, $model->class,$model->$key)
                     ];
                 }
             }
+
             $this->viewService->setVar('content',
                 $this->eventsManager->fire(ViewEnum::RENDER_TEMPLATE_EVENT,new RenderTemplateDTO(
                     'adminModelReadOnly',
                     '',
-                    [
-                        'model' => $model,
-                        'properties' => $vars
-                    ]
+                    ['model' => $model, 'properties' => $vars]
             )));
         }
+    }
+
+    private function getReadOnlyValue(string $key, string $class, string|object $value):string
+    {
+        if (gettype($value) === 'object') {
+            if ($value::class === 'MongoDB\BSON\UTCDateTime') {
+                $value = $value->toDateTime()->format('Y-m-d H:i:s');
+            }
+        }
+        switch ($key) {
+            case 'itemId':
+                if(!empty($class)) {
+                    $eventTrigger = array_reverse(explode('\\',$class))[0].'Listener:getRepository';
+                    $repository = $this->eventsManager->fire($eventTrigger, new \stdClass());
+                    if($repository !== null) {
+                        $item = $repository->getById((string)$value, false);
+                        if ($item !== null) {
+                            $value = $item->getNameField().' ( '.$value.' )';
+                        }
+                    }
+                }
+                break;
+            case 'userId':
+                $user = $this->userRepository->getById((string) $value, false);
+                if($user !== null) {
+                    $value = $user->getNameField().' ( '.$value.' )';
+                }
+                break;
+        }
+
+        return $value;
     }
 }
