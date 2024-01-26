6ac5d0c3a991365573876e2dbecc42d32a290730
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-01-26 11:37:33 +0100

    Added AdminListButtonFactoryTest.php

---
+++ tests/Blocks/ToolbarTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VitesseCms\Admin\Tests\Blocks;
+
+use Phalcon\Mvc\View;
+use PHPUnit\Framework\TestCase;
+use VitesseCms\Admin\Blocks\Toolbar;
+use VitesseCms\Block\Models\Block;
+use VitesseCms\Core\Services\BootstrapService;
+use VitesseCms\Core\Services\ViewService;
+
+class ToolbarTest extends TestCase
+{
+    public function testToolbarParams()
+    {
+        //finish when abstract block is implified
+    }
+}
---
+++ tests/Factories/AdminListButtonFactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VitesseCms\Admin\Tests\Factories;
+
+use PHPUnit\Framework\TestCase;
+use VitesseCms\Admin\Factories\AdminListButtonFactory;
+use VitesseCms\Admin\Models\AdminListButton;
+
+class AdminListButtonFactoryTest extends TestCase
+{
+    public function testCreate()
+    {
+        $adminListButton = AdminListButtonFactory::create('openmodal','http://test.nl','Test website');
+        $this->assertInstanceOf(AdminListButton::class, $adminListButton);
+        $this->assertSame('openmodal',$adminListButton->cssClass);
+        $this->assertSame('http://test.nl',$adminListButton->href);
+        $this->assertSame('Test website',$adminListButton->title);
+    }
+
+}
