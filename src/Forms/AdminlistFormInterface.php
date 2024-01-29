340f71813b10755757bcfd2e72733d4a9ba3c50c
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2024-01-29 12:12:04 +0100

    Cleanup code

--- src/Forms/AdminlistFormInterface.php
+++ src/Forms/AdminlistFormInterface.php
@@ -1,12 +1,12 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace VitesseCms\Admin\Forms;
 
-use VitesseCms\Form\Interfaces\AbstractFormInterface;
-
 interface AdminlistFormInterface
 {
-    public function addNameField(AbstractFormInterface $form): void;
+    public function addNameField(): void;
 
-    public function addPublishedField(AbstractFormInterface $form): void;
+    public function addPublishedField(): void;
 }
